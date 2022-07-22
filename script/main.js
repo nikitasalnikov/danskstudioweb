03cfbe463c3acc5357b40a086fb37136de2d1669
nikita <nikitasalnikov@yandex.ru>
2022-07-22 17:24:52 +0500

    commit

--- script/main.js
+++ script/main.js
@@ -22,56 +22,106 @@ const price = document.querySelector('.price');
 const lessonDuration = document.querySelector('.lessons__duration');
 const modalForm = document.querySelector('.modal__window-form');
 const selectInput = document.querySelector('.select__input');
-const selectItem = document.querySelector('.__select__content')
+const selectItem = document.querySelector('.__select__content');
+const select = document.querySelector('#select_id');
 
-var priceArray = [2000, 600, 800, 1000, 3000, 500];
+
+
+var priceArray = [2000, 600, 800, 1000, 1500, 3000, 500];
 
 for (let i = 0; i < selectSingle_labels.length; i++) {
+    
+    
     selectSingle_labels[i].addEventListener('click', function () {
-        if (selectSingle_labels[i].getAttribute('data-value') == 'Авторский курс по произношению “Основы произношения датского языка. Постановка основных звуков”' || selectSingle_labels[i].getAttribute('data-value') == 'Авторский курс по произношению “Нюансы произношения датского языка. Фокус на гласные”"' || selectSingle_labels[i].getAttribute('data-value') == 'Авторский курс по произношению “Мелодия и интонация датского языка. Вишенка на торте.”') {
+
+        if (selectSingle_labels[i].getAttribute('data-value') == 'Авторский курс по произношению “Основы произношения датского языка. Постановка основных звуков”') {
+            price.innerHTML = `Стоимость курса: ${priceArray[0]} крон`;
             selectInput.classList.remove('active');
-            lessonDuration.innerHTML = 'Продолжительность: 5 недель, 2 урока в неделю. '
-            price.innerHTML = `Стоимость курса: ${priceArray[0]} крон`
-        } else if (selectSingle_labels[i].getAttribute('data-value') == 'ПОДГОТОВКА К ЭКЗАМЕНАМ') {
+        } 
+        else if (selectSingle_labels[i].getAttribute('data-value') == 'Авторский курс по произношению “Нюансы произношения датского языка. Фокус на гласные”') {
+            price.innerHTML = `Стоимость курса: ${priceArray[0]} крон`;
+            selectInput.classList.remove('active');
+        } 
+        else if (selectSingle_labels[i].getAttribute('data-value') == 'Авторский курс по произношению “Мелодия и интонация датского языка. Вишенка на торте.”') {
+            price.innerHTML = `Стоимость курса: ${priceArray[0]} крон`;
+            selectInput.classList.remove('active');
+        } 
+        else if (selectSingle_labels[i].getAttribute('data-value') == 'ПОДГОТОВКА К ЭКЗАМЕНАМ' || selectSingle_labels[i].getAttribute('data-value') == 'ПОСТАНОВКА ПРОИЗНОШЕНИЯ' || selectSingle_labels[i].getAttribute('data-value') == 'ЯЗЫКОВОЙ КОУЧИНГ') {
+            price.innerHTML = '';
             selectInput.classList.add('active');
-            lessonDuration.innerHTML = 'Продолжительность: индивидуальное количество, уроки 60-70мин'
-            price.innerHTML = `Стоимость курса: ${priceArray[1]} крон за занятие`
             selectItem.style = `
             max-width:780px;
             width:100%;
             `
-        } else if (selectSingle_labels[i].getAttribute('data-value') == 'ПОСТАНОВКА ПРОИЗНОШЕНИЯ' || selectSingle_labels[i].getAttribute('data-value') == 'ЯЗЫКОВОЙ КОУЧИНГ') {
+            if (select) {
+                select.addEventListener('change', function() {
+                    if (select.value == 0) {
+                        price.innerHTML = '';
+                    }
+                    else if(select.value == 1){
+                        price.innerHTML = `Стоимость курса: ${priceArray[1]} крон`;
+                    }
+                   else if(select.value == 2 || select.value == 3){
+                        price.innerHTML = `Стоимость курса: ${priceArray[2]} крон`;
+                    }
+                })
+            }
+        } 
+        else if (selectSingle_labels[i].getAttribute('data-value') == 'ДАТСКИЙ С НУЛЯ' || selectSingle_labels[i].getAttribute('data-value') == 'Клуб интерактивного датского языка для продолжающих') {
             selectInput.classList.add('active');
-            lessonDuration.innerHTML = 'Продолжительность: индивидуальное количество, уроки 60-70мин'
-            price.innerHTML = `Стоимость курса: ${priceArray[2]} крон за занятие`
-            selectItem.style = `
-            max-width:780px;
-            width:100%;
-            `
-        } else if (selectSingle_labels[i].getAttribute('data-value') == 'ДАТСКИЙ С НУЛЯ' || selectSingle_labels[i].getAttribute('data-value') == 'Клуб интерактивного датского языка для продолжающих') {
-            selectInput.classList.add('active');
-            lessonDuration.innerHTML = 'Продолжительность: индивидуальное количество, уроки 60-70мин';
             price.innerHTML = `Стоимость курса: ${priceArray[3]} крон/в месяц`;
             selectItem.style = `
             max-width:780px;
             width:100%;
             `
-        } else if (selectSingle_labels[i].getAttribute('data-value') == 'Коучинговая группа по постановке произношения') {
+            if (select) {
+                select.addEventListener('change', function() {
+                    if (select.value == 0) {
+                        price.innerHTML = '';
+                    }
+                    else if(select.value == 1 || select.value == 2 || select.value == 3){
+                        price.innerHTML = `Стоимость курса: ${priceArray[3]} крон/в месяц`;
+                    }
+                })
+            }
+           
+        } 
+        else if (selectSingle_labels[i].getAttribute('data-value') == 'Коучинговая группа по постановке произношения') {
             selectInput.classList.add('active');
-            lessonDuration.innerHTML = 'Продолжительность: индивидуальное количество, уроки 60-70мин';
             price.innerHTML = `Стоимость курса: ${priceArray[4]} крон`;
             selectItem.style = `
             max-width:780px;
             width:100%;
             `
+            if (select) {
+                select.addEventListener('change', function() {
+                    if (select.value == 0) {
+                        price.innerHTML = '';
+                    }
+                    else if(select.value == 1 || select.value == 2 || select.value == 3){
+                        price.innerHTML = `Стоимость курса: ${priceArray[4]} крон/в месяц`;
+                    }
+                })
+            }
+
         } else if (selectSingle_labels[i].getAttribute('data-value') == 'КИНОКЛУБ') {
             selectInput.classList.add('active');
-            lessonDuration.innerHTML = 'Продолжительность: индивидуальное количество, уроки 60-70мин';
             price.innerHTML = `Стоимость курса: ${priceArray[5]} крон/в месяц`;
             selectItem.style = `
             max-width:780px;
             width:100%;
             `
+            if (select) {
+                select.addEventListener('change', function() {
+                    if (select.value == 0) {
+                        price.innerHTML = '';
+                    }
+                    else if(select.value == 1 || select.value == 2 || select.value == 3){
+                        price.innerHTML = `Стоимость курса: ${priceArray[3]} крон/в месяц`;
+                    }
+                })
+            }
+
         }
     })
 }
